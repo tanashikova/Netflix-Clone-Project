bdea2e81e6b10b283e11e17215ade89b4f80ae18
DinaDina <59812601+tanashikova@users.noreply.github.com>
2021-02-28 21:40:22 -0800

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('../config/ppConfig')
+const db = require("../models")
+const methodOverride = require('method-override');
+
+router.use(methodOverride('_method'));
+
+router.get('/signup', (req, res) => {
+  res.render('auth/signup');
+});
+
+router.post('/signup', (req, res) => {
+  // find or create the user
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).then(([user, created]) => {
+    if (created) {
+      // success
+      passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        // successFlash: 'Account created and user logged in!'
+      })(req, res)
+    } else {
+      // user already exists, so we redirect
+      req.flash('error', 'Email already exists')
+      res.redirect('/auth/signup')
+    }
+  }).catch(error => {
+    // if an error occurs, console log the error message
+    req.flash('error', error.message)
+    res.redirect('/auth/signup')
+  })
+})
+
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/dashboard',
+  failureRedirect: '/auth/login',
+  // successFlash: 'You have logged in!',
+  failureFlash: 'Invalid username and/or password.'
+}))
+
+// update route
+router.put('/update', (req, res) => {
+  // find or create the user
+  console.log('req', req.body)
+  db.user.update({
+    email: req.body.email,
+    name: req.body.name,
+    password: req.body.password
+  },{
+  where: {
+    id: parseInt(req.body.id)
+  }
+  }).then((user) => {
+    console.log('user: ', user )
+    if (user) {
+      // success
+      req.flash('success', 'Account succesfully updated!')
+      res.redirect('/profile')
+    } else {
+      // user already exists, so we redirect
+      req.flash('error', 'Something went wrong')
+      res.redirect('/profile')
+    }
+  }).catch(error => {
+    // if an error occurs, console log the error message
+    req.flash('error', error.message)
+    res.redirect('/profile')
+  })
+})
+
+router.get('/logout', (req, res) => {
+  // .logout() is added to the req object by passport
+  req.logout()
+  // req.flash('success', 'You have logged out!')
+  res.redirect('/auth/signup')
+})
+
+router.delete('/:id', function(req, res) {
+  db.user.destroy({
+    where: { id: parseInt(req.body.id)}
+  }).then(function(removed) {
+    res.redirect('/auth/signup')
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,163 @@
+const requests = require('./middleware/requests');
+const axios = require('./middleware/axios');
+// requiring database
+const db = require("./models");
+// fetching requests
+const trending = require('./middleware/trending');
+const originals = require('./middleware/originals');
+const topRated = require('./middleware/topRated');
+const family = require('./middleware/family');
+const animated = require('./middleware/animated');
+const romance = require('./middleware/romance');
+const comedy = require('./middleware/comedy');
+const action = require('./middleware/action');
+const banner = require('./middleware/banner');
+
+
+require('dotenv').config();
+const express = require('express');
+const layouts = require('express-ejs-layouts');
+const session = require('express-session');
+const flash = require("connect-flash")
+const passport = require('./config/ppConfig');
+const isLoggedIn = require('./middleware/isLoggedIn')
+const methodOverride = require('method-override');
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(require("morgan")("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
+app.use(layouts);
+
+app.use(
+  session({
+    // a string used to generate a unique
+    // session ID cookie to share with the browser
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// the following two lines must appear after configuring the session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// FLASH
+app.use(flash());
+// adds a method to the req object for universal access
+
+// Set up local variables (data that's accessible from anywhere in the app)
+app.use((req, res, next) => {
+  // before every route is loaded, attach flash messages and the
+  // current user to res.locals
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+
+  next();
+});
+// grabbing movie data
+
+app.get('/dashboard', async (req, res) => {
+  const grabBanner = await banner.fetchBanner()
+  const grabTrending = await trending.fetchTrend()
+  const grabOriginals = await originals.fetchOriginals()
+  const grabTopRated = await topRated.fetchTopRated()
+  const grabFamily = await family.fetchFamily()
+  const grabAnimated = await animated.fetchAnimated()
+  const grabRomance = await romance.fetchRomance()
+  const grabComedy = await comedy.fetchComedy()
+  const grabAction = await action.fetchAction()
+  res.render('index', {
+    myBanner: grabBanner,
+    myTrending: grabTrending,
+    myOriginals: grabOriginals,
+    myTopRated: grabTopRated,
+    myFamily: grabFamily,
+    myAnimated: grabAnimated,
+    myRomance: grabRomance,
+    myComedy: grabComedy,
+    myAction: grabAction
+  });
+});
+let myMovieRes = []
+app.get("/search/:search", (req, res)=>{
+  let keyword  = req.query.title
+  axios.get(`${requests.searchMovie}${keyword}`)
+   .then((responseData)=>{
+     const searchData = responseData.data.results
+     searchData.forEach((movie)=>{ 
+       if(movie.poster_path!= null){
+          myMovieRes.push(movie)
+     } 
+  }) 
+  res.render("search", {responseData: myMovieRes})
+  myMovieRes = []
+  })
+})
+
+
+app.get("/profile", isLoggedIn, (req, res) => {
+  res.render("profile");
+});
+
+// POST go to watch now view
+app.post("/watch", (req, res) => {
+    res.render('watch',{watchNowInput: req.body.watchNowInput})
+  // })
+});
+
+// POST create a join action
+app.post("/watch-later", (req, res) => {
+  console.log('req.body',req.body)
+  db.watchList.findOrCreate({
+    where: {
+      userId: req.body.userId,
+      movieId: req.body.videoId,
+      movieName: req.body.videoName,
+      movieDescription: req.body.overviewInput,
+      movieRating: req.body.ratingInput,
+      movieImg: req.body.imgInput
+    }
+  }).then(function(join) {
+    res.redirect('/watch-later')
+  })
+});
+// GET favorite movies
+app.get('/watch-later', function(req, res) {
+  // Our database calls are added WITHIN the route as seen here
+  db.watchList.findAll().then((getList) => {
+    // We respond to the original request within the DB callback function and send some data to a template
+    res.render('watchLater', { watchLater: getList })
+  })
+});
+
+//delete movie from watchlist
+app.delete ("/watch-later", function (req, res) {
+  const movieId = req.body.movieId;
+  const userId = req.body.userId;
+  db.watchList.destroy({
+    where: { movieId: movieId,
+            userId: userId
+          }
+  }).then(function(removed) {
+     res.redirect('/watch-later')
+  });
+  
+});
+
+app.use("/auth", require("./routes/auth"));
+
+var server = app.listen(process.env.PORT || 3000, () =>
+  console.log(
+    `🎧You're listening to the smooth sounds of port ${
+      process.env.PORT || 3000
+    }🎧`
+  )
+);
+
+module.exports = server;
