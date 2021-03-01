f46c333c3e01b19587a33b8534af83cf11717360
didin21 <tanashikovadina@gmail.com>
2021-02-28 23:19:45 -0800

    fixes main page issue

--- routes/auth.js
+++ routes/auth.js
@@ -6,11 +6,11 @@ const methodOverride = require('method-override');
 
 router.use(methodOverride('_method'));
 
-router.get('/signup', (req, res) => {
+router.get('/', (req, res) => {
   res.render('auth/signup');
 });
 
-router.post('/signup', (req, res) => {
+router.post('/', (req, res) => {
   // find or create the user
   db.user.findOrCreate({
     where: { email: req.body.email },
@@ -82,14 +82,14 @@ router.get('/logout', (req, res) => {
   // .logout() is added to the req object by passport
   req.logout()
   // req.flash('success', 'You have logged out!')
-  res.redirect('/auth/signup')
+  res.redirect('/')
 })
 
 router.delete('/:id', function(req, res) {
   db.user.destroy({
     where: { id: parseInt(req.body.id)}
   }).then(function(removed) {
-    res.redirect('/auth/signup')
+    res.redirect('/')
   });
 });
 
